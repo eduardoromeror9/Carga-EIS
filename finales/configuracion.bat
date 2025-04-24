d62f4fb4abe4dcedc5fcba1ca766bf66fc1dd3b1
Eduardo Romero <eduardoromeror9@outlook.com>
2025-04-24 03:30:16 -0400

    git ignore agregado y md modificado

--- finales/configuracion.bat
+++ finales/configuracion.bat
@@ -5,7 +5,7 @@ REM ###### CONFIGURACIÓN GENERAL #######
 set ANHO_MES=250400
 set DIA_INICIO=21
 set DIA_FIN=21
-set TIEMPO_ESPERA=30
+set TIEMPO_ESPERA=40
 
 REM ####################################
 

0ae5301350ebc4bb8b38c4f36aeb848bff55ca0a
Eduardo Romero <eduardoromeror9@outlook.com>
2025-04-24 02:08:27 -0400

    proyecto para la automatización del reproceso EIS

---
+++ finales/finEIS.bat
@@ -0,0 +1,26 @@
+REM Author: Eduardo Romero
+REM Date: 2025-04-01
+REM Description: Script para ejecutar carga y calculo EIS
+
+@echo off
+setlocal enabledelayedexpansion
+
+REM Cargar archivo de configuración
+call configuracion.bat
+
+for /L %%d in (%DIA_INICIO%,1,%DIA_FIN%) do (
+    set /A fecha=%ANHO_MES% + %%d
+
+    echo #######################################################
+    echo Ejecutando procesos para el día %%d → Fecha: !fecha!
+
+    echo Ejecutando Carga: !fecha!
+    perl cargar.pl -C1 !fecha!
+    timeout /t %TIEMPO_ESPERA% /nobreak >nul
+
+    echo Ejecutando Calculo: !fecha!
+    perl calcular.pl !fecha!
+    timeout /t %TIEMPO_ESPERA% /nobreak >nul
+)
+
+endlocal
---
+++ pruebas/main_hola.py
@@ -0,0 +1,7 @@
+# Esta funcion solo es una prueba para ver el comportamiento de de la shell
+def muestra_ejecucion(texto):
+    texto_invertido = texto[::-1]
+    print(texto_invertido)
+texto_usuario = ('Palabra')
+print(texto_usuario)
+muestra_ejecucion(texto_usuario)
